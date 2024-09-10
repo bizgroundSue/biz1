bd154e85b61e75e0d7b0fbeabe50e850070afb7d
suhyeon <suhyeon@192.168.219.58>
2024-09-10 20:02:19 +0900

    240910 프로젝트 생성

---
+++ Biz1/src/main/java/com/biz/Biz1Application.java
@@ -0,0 +1,13 @@
+package com.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Biz1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Biz1Application.class, args);
+	}
+
+}
---
+++ Biz1/src/test/java/com/biz/Biz1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Biz1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
