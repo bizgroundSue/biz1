f8e341c19c1f1470d512d263d9b846eb6eb2ff12
suhyeon <suhyeon@192.168.219.58>
2024-09-13 17:44:26 +0900

    240913 1개주문하기 페이지 생성 및 controller에서 데이터 받아오기

--- Biz1/src/main/java/com/biz/controller/OrderController.java
+++ Biz1/src/main/java/com/biz/controller/OrderController.java
@@ -2,6 +2,10 @@ package com.biz.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.biz.entity.Order;
 
 @Controller
 public class OrderController {
@@ -10,4 +14,13 @@ public class OrderController {
 	public String orderMain() {
 		return "OrderMain";
 	}
+	
+	@PostMapping("/order")
+	public String order(Order order) {
+		System.out.println("=========================order Method Call ");
+		System.out.println(order.toString());
+		
+		return "";
+	}
+	
 }
