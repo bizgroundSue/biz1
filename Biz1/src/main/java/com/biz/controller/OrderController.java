6fb3b232fab1de491718f88bdca2d063ded83d89
suhyeon <suhyeon@192.168.219.58>
2024-09-13 17:17:11 +0900

    240913 브런치 생성 및 주문하기 페이지 생성

--- Biz1/src/main/java/com/biz/controller/OrderController.java
+++ Biz1/src/main/java/com/biz/controller/OrderController.java
@@ -1,5 +1,13 @@
 package com.biz.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class OrderController {
 
+	@GetMapping("/orderMain")
+	public String orderMain() {
+		return "OrderMain";
+	}
 }
--- Biz1/src/main/java/com/biz/entity/Order.java
+++ Biz1/src/main/java/com/biz/entity/Order.java
@@ -1,5 +1,31 @@
 package com.biz.entity;
 
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="TB_ORDER")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+	
+	@Id
+	private int order_id;
+	private String user_id;
+	private String cafe_id;
+	private Date time;
+	private String menu_name;
+	private int quantities;
+	private String payment;
+	private String status;
 
 }
